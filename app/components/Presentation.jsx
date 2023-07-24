5e4add36af08c2c79c215c13b2c16c877575399d
JoseALopezP <jalp42@live.com>
2023-07-24 14:17:47 -0300

    Corrections

--- app/components/Presentation.jsx
+++ app/components/Presentation.jsx
@@ -3,9 +3,11 @@ import styles from './Presentation.module.css'
 export function Presentation () {
     return (
         <>
-            <p>¡Hola! Soy</p>
-            <h3>José López</h3>
-            <p>&lt;<strong>Web Developer</strong>/&gt;</p>
+            <div className={`${styles.holaSoy}`}>
+                <p>¡Hola! Soy</p>
+                <h3>José López</h3>
+                <p><strong>&lt;</strong>Web Developer<strong>/&gt;</strong></p>
+            </div>
         </>
     )
 }
