4faaf52fe6d9394d1e50e6e525490279eb89ca3c
JoseALopezP <jalp42@live.com>
2023-08-02 12:50:23 -0300

    At least i know now what is wrong

--- app/components/Presentation.jsx
+++ app/components/Presentation.jsx
@@ -6,7 +6,9 @@ export function Presentation () {
             <div className={`${styles.presentationBlock}`}>
                 <div className={`${styles.presentationTextBlock}`}>
                     <p className={`${styles.presentationText1}`}>¡Hola! Soy</p>
-                    <h3 className={`${styles.presentationText2}`}>José López</h3>
+                    <div className={`${styles.typewritterBlock} ${styles.presentationText2}`}>
+                        <h3 className={`${styles.presentationText2}`}>José López</h3>
+                    </div>
                     <p className={`${styles.presentationText3}`}>&lt;<strong> Web Developer </strong>/&gt;</p>
                 </div>
             </div>
