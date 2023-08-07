d4ee95e2b2244c1334f66ab65fcc938fbc463b33
JoseALopezP <jalp42@live.com>
2023-08-07 14:36:08 -0300

    Added ampping for technologies

--- app/components/Tecnhologies.jsx
+++ app/components/Tecnhologies.jsx
@@ -2,7 +2,41 @@ import styles from './AboutMe.module.css'
 import Image from 'next/image'
 
 const technologiesList = [{
-    
+    name: 'JavaScript',
+    route: '/public/images/icons/.svg',
+    name: '',
+},{
+    name: 'React.js',
+    route: '/public/images/icons/.svg',
+    name: '',
+},{
+    name: 'HTML',
+    route: '/public/images/icons/.svg',
+    name: '',
+},{
+    name: 'CSS',
+    route: '/public/images/icons/.svg',
+    name: '',
+},{
+    name: 'Node.js',
+    route: '/public/images/icons/.svg',
+    name: '',
+},{
+    name: 'Firebase',
+    route: '/public/images/icons/.svg',
+    name: '',
+},{
+    name: 'Sass',
+    route: '/public/images/icons/.svg',
+    name: '',
+},{
+    name: 'Git',
+    route: '/public/images/icons/.svg',
+    name: '',
+},{
+    name: 'Next.js',
+    route: '/public/images/icons/.svg',
+    name: '',
 }]
 
 export function AboutMe () {
