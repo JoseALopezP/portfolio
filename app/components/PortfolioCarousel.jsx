1acfc45e1cfb1b294ee49bc38084204603c8b661
JoseALopezP <jalp42@live.com>
2023-08-10 21:30:18 -0300

    Struggling with keen slider

--- app/components/PortfolioCarousel.jsx
+++ app/components/PortfolioCarousel.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react"
-import { useKeenSlider } from "keen-slider/react"
-import "keen-slider/keen-slider.min.css"
-import "./styles.css"
+'use client'
+import React, { useState } from 'react';
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react'
+import styles from './Portfolio.module.css'
 
 export function PortfolioCarousel () {
   const [currentSlide, setCurrentSlide] = useState(0)
@@ -18,34 +19,36 @@ export function PortfolioCarousel () {
 
   return (
     <>
-      <div className="navigation-wrapper">
-        <div ref={sliderRef} className="keen-slider">
-          <div className="keen-slider__slide number-slide1">1</div>
-          <div className="keen-slider__slide number-slide2">2</div>
-          <div className="keen-slider__slide number-slide3">3</div>
-          <div className="keen-slider__slide number-slide4">4</div>
-          <div className="keen-slider__slide number-slide5">5</div>
-          <div className="keen-slider__slide number-slide6">6</div>
+      <div className={`${styles.carouselBlock} navigation-wrapper`}>
+        <div ref={sliderRef} className={`${styles.slidesblock} keen-slider`}>
+          <div className={`${styles.slide} keen-slider__slide number-slide1`}>1</div>
+          <div className={`${styles.slide} keen-slider__slide number-slide2`}>2</div>
+          <div className={`${styles.slide} keen-slider__slide number-slide3`}>3</div>
+          <div className={`${styles.slide} keen-slider__slide number-slide4`}>4</div>
+          <div className={`${styles.slide} keen-slider__slide number-slide5`}>5</div>
+          <div className={`${styles.slide} keen-slider__slide number-slide6`}>6</div>
         </div>
         {loaded && instanceRef.current && (
           <>
-            <Arrow
-              left
-              onClick={(e) =>
-                e.stopPropagation() || instanceRef.current?.prev()
-              }
-              disabled={currentSlide === 0}
-            />
+            <div className={`${styles.arrows}`}>
+              <Arrow
+                left
+                onClick={(e) =>
+                  e.stopPropagation() || instanceRef.current?.prev()
+                }
+                disabled={currentSlide === 0}
+              />
 
-            <Arrow
-              onClick={(e) =>
-                e.stopPropagation() || instanceRef.current?.next()
-              }
-              disabled={
-                currentSlide ===
-                instanceRef.current.track.details.slides.length - 1
-              }
-            />
+              <Arrow
+                onClick={(e) =>
+                  e.stopPropagation() || instanceRef.current?.next()
+                }
+                disabled={
+                  currentSlide ===
+                  instanceRef.current.track.details.slides.length - 1
+                }
+              />
+            </div>
           </>
         )}
       </div>
--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
 import { Presentation } from "./components/presentation";
 import { AboutMe } from "./components/AboutMe";
+import { Portfolio } from "./components/Portfolio";
 
 export default function Home() {
   return (
     <main>
       <Presentation/>
       <AboutMe/>
+      <Portfolio/>
     </main>
   )
 }
