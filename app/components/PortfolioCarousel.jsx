5091a587cc1c527931d89c92ffc5435c25137762
JoseALopezP <jalp42@live.com>
2023-09-01 00:26:43 -0300

    Going forward with fear

--- app/components/PortfolioCarousel.jsx
+++ app/components/PortfolioCarousel.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, {Component} from 'react';
+import React from 'react';
 import styles from './Portfolio.module.css';
 import Image from 'next/image';
 import CarouselSlide from './CarouselSlide.jsx';
