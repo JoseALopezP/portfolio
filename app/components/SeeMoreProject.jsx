67bfb497fb5b1fa5e7d5d3e6fbf7900f12cdbd9d
JoseALopezP <jalp42@live.com>
2024-12-21 19:04:08 -0300

    new beginnings

--- app/components/SeeMoreProject.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Portfolio.module.css'
-
-export function SeeMoreProject (proj) {
-    return (
-        <div className={styles.projectCardBlock}>
-            <h5>{`${proj.proj.pName}`}</h5>
-        </div>
-    )
-}
